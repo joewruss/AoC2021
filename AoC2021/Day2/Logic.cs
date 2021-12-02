1815bc702169c0881ddfc367696cc46c67a0a711
Joe Russ <joseph.russ@cfainstitute.org>
2021-12-02 00:18:22 -0500

    Day 2 solutions

--- AoC2021/Day2/Logic.cs
+++ AoC2021/Day2/Logic.cs
@@ -8,14 +8,58 @@ namespace AoC2021.Day2
 {
     public class Logic : ILogic
     {
-        public Task<string> RunPart1(string fileName)
+        public async Task<string> RunPart1(string fileName)
         {
-            return Task.FromResult("not implemented");
+            var instructions = await Tools.InputParser.GetInputAsEnumerable<Instruction>(fileName, (t) => { return new Instruction(t) ; });
+
+            int depth = 0;
+            int distance = 0;
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Direction == "forward") distance = distance + instruction.Amount;
+                if (instruction.Direction == "up") depth = depth - instruction.Amount;
+                if (instruction.Direction == "down") depth = depth + instruction.Amount;
+
+            }
+
+            return (depth * distance).ToString();
+
         }
 
-        public Task<string> RunPart2(string fileName)
+        public async Task<string> RunPart2(string fileName)
         {
-            return Task.FromResult("not implemented");
+            var instructions = await Tools.InputParser.GetInputAsEnumerable<Instruction>(fileName, (t) => { return new Instruction(t); });
+
+            int depth = 0;
+            int horizontalDistance = 0;
+            int aim = 0;
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Direction == "forward")
+                {
+                    horizontalDistance = horizontalDistance + instruction.Amount;
+                    depth = depth + (aim * instruction.Amount);
+                }
+                if (instruction.Direction == "up") aim = aim - instruction.Amount;
+                if (instruction.Direction == "down") aim = aim + instruction.Amount;
+
+            }
+
+            return (depth * horizontalDistance).ToString();
         }
+    }
+
+    public class Instruction
+    {
+        public Instruction(string line)
+        {
+            Direction = line.Substring(0, line.IndexOf(" "));
+            Amount = int.Parse(line.Substring(line.IndexOf(" ")));
+        }
+        
+        public string Direction { get; set; }
+        public int Amount { get; set; }
     }
 }
