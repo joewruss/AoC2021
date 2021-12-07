a5086d93e3d17de0e0bd1da0911c9a4d9f9508aa
Joe Russ <joseph.russ@cfainstitute.org>
2021-12-07 10:38:13 -0500

    Day 7

--- AoC2021/Day7/Logic.cs
+++ AoC2021/Day7/Logic.cs
@@ -7,14 +7,39 @@ namespace AoC2021.Day7
 {
     public class Logic : ILogic
     {
-        public Task<string> RunPart1(string fileName)
+        public async Task<string> RunPart1(string fileName)
         {
-            return Task.FromResult("not implemented");
+            var positions = await Tools.InputParser.GetFirstLineAsEnumerable<int>(fileName, "," ,l => { return int.Parse(l); });
+            var fuelUsage = new Dictionary<int, int>();
+            for (int i = positions.Min(); i <= positions.Max(); i++)
+            {
+                var usage = 0;
+                foreach(var p in positions)
+                {
+                    usage = usage + Math.Abs(i - p);
+                }
+                fuelUsage.Add(i, usage);
+            }
+
+            return fuelUsage.OrderBy(fu => fu.Value).First().Value.ToString();
         }
 
-        public Task<string> RunPart2(string fileName)
+        public async Task<string> RunPart2(string fileName)
         {
-            return Task.FromResult("not implemented");
+            var positions = await Tools.InputParser.GetFirstLineAsEnumerable<int>(fileName, ",", l => { return int.Parse(l); });
+            var fuelUsage = new Dictionary<int, int>();
+            for (int i = positions.Min(); i <= positions.Max(); i++)
+            {
+                int usage = 0;
+                foreach (var p in positions)
+                {
+                    double a = Math.Abs(i - p);
+                    usage = (int)(usage + ((a * a) / 2) + (a/2));
+                }
+                fuelUsage.Add(i, usage);
+            }
+
+            return fuelUsage.OrderBy(fu => fu.Value).First().Value.ToString();
         }
     }
 }
